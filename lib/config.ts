7f25905510fb9fcd838826d761be0b7a744928dc
Adhul Sanjay <adhulcsanjay1728@gmail.com>
2026-01-08 01:35:58 +0530

    change

--- lib/config.ts
+++ lib/config.ts
@@ -1,17 +1,4 @@
 
-// function getEnv(key: string): string {
-//   const value = process.env[key]
-
-//   if (!value) {
-//     throw new Error(`Missing environment variable: ${key}`)
-//   }
-
-//   return value
-// }
-
-// export const CONFIG = {
-//   API_BASE_URL: getEnv('NEXT_PUBLIC_API_BASE_URL'),
-// }
 
 
 export const CONFIG = {
